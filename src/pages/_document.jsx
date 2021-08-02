46ef2ef9a8c065e2bb1f1958a86fa0aee262e6c1
Robson Luiz <robsonptrainer@gmail.com>
2021-08-02 20:24:03 -0300

    fix buffer private key

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -10,11 +10,12 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel='stylesheet' href='/_next/static/globals.css' />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;800&display=swap" rel="stylesheet" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,900;1,400&display=swap" rel="stylesheet" />
         </Head>
         <body>
